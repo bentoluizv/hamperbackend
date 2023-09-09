0c29cdcb19d7228dbc24ad546b2acd36d52b62a7
IgorStrauss <engenheirostrauss2@gmail.com>
2023-09-08 21:47:58 -0300

    Atualização db development para sqlite

--- databases/development_config.py
+++ databases/development_config.py
@@ -4,6 +4,6 @@ from flask import Config
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DB}"
+    SQLALCHEMY_DATABASE_URI = settings.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = settings.get('SECRET_KEY')
