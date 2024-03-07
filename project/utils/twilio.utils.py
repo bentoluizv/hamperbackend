f6487bec4f1f00f136da6b4721a42e66ff595bf3
Vanessa Moura <nessa.moura8@gmail.com>
2024-03-07 14:49:00 -0300

    poc: refactor twilio utils

--- project/utils/twilio.utils.py
+++ project/utils/twilio.utils.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
 
-def send_message(new_order):
+def send_whatsapp_message(new_order):
   account_sid = 'AC07bda34115f9e874de261be356af10d4'
   auth_token = '14233dfe02c685d45df2c26cd2438e25'
   client = Client(account_sid, auth_token)
