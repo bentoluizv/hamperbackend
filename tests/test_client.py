dda32af27fdfa7bd409e5e26b33a4e739e890b89
felipe <f.marinho162001@gmail.com>
2024-03-12 12:50:17 -0300

    feat: criando testes de integração dos métodos POST

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,4 +3,21 @@ def test_list_client_return_200(app_testing):
     response = client.get('http://127.0.0.1:5000/api/v1/clients/')
     assert response.status_code == 200
 
+def test_post_client_return_200(app_testing):
+    client = app_testing.test_client()
+
+    client_data = {
+        "client_name": "Maria Oliveira",
+        "client_cellphone": "69996666666",
+        "client_address": "Rua do Teste, 123",
+        "client_address_number": 123,
+        "client_address_complement": "Apto 101",
+        "client_address_neighborhood": "Bairro do Teste",
+        "client_zip_code": "12345-678"
+    }
+
+    response = client.post('/api/v1/clients/', json=client_data)
+   
+    assert response.status_code == 201
+
 
--- tests/test_order.py
+++ tests/test_order.py
@@ -2,3 +2,17 @@ def test_list_order_return_200(app_testing):
     order = app_testing.test_client()
     response = order.get('http://127.0.0.1:5000/api/v1/orders/')
     assert response.status_code == 200
+
+def test_post_order_return_200(app_testing):
+    client = app_testing.test_client()
+
+    order_data = {
+        "client_id": 4,
+        "restaurant_id": 5,
+        "products": [5],
+        "created_at": "2024-03-11 10:00:00"
+    }
+
+    response = client.post('/api/v1/orders/', json=order_data)
+   
+    assert response.status_code == 201
