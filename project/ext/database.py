3c19af30eec65448af05e8424e807c2c7a70f33a
jvras <jonathas.vincius@ufpe.br>
2024-04-19 18:55:12 -0300

    WIP: Corrigir erros de chave estrangeira e restrições not null nos controladores e modelos nos ambientes de homologação e produção

--- project/ext/database.py
+++ project/ext/database.py
@@ -1,8 +1,18 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlite3 import Connection as SQLite3Connection
 
 db = SQLAlchemy()
 
+# FIXME: ADD THIS FUNCTION DEVERIA SER O BASTANTE PARA CORRIGIR O PROBLEMA DA FK NO SQLITE
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
 
 def init_app(app):
     db.init_app(app)
