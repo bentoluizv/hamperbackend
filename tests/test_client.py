1dbe658b287b63181ab6728633fa7543bade7ba0
Vanessa Moura <nessa.moura8@gmail.com>
2024-06-26 21:43:50 -0300

    fix redis

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,12 @@
-def test_list_client_return_200(app_testing):
-    client = app_testing.test_client()
-    response = client.get('http://127.0.0.1:5000/api/v1/clients/')
-    assert response.status_code == 200
+from unittest.mock import patch
+
+
+def test_list_client_return_200_when_redis_is_empty(app_testing, fake_redis):
+    with patch('project.utils.redis_utils.get_redis_value', return_value=fake_redis):
+        client = app_testing.test_client()
+        response = client.get('http://127.0.0.1:5000/api/v1/clients/')
+        assert response.status_code == 200 
+
 
 def test_post_client_return_200(app_testing):
     client = app_testing.test_client()
